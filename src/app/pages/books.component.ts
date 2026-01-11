7ad4236b251d784cd8635fe7eacb9ef8589a2bbe
atikahadzman <atikah.adzman@gmail.com>
2026-01-12 00:19:59 +0800

    books filter by title or author

--- src/app/pages/books.component.ts
+++ src/app/pages/books.component.ts
@@ -4,8 +4,11 @@ import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { BookService } from '../services/book.service';
 import { Book } from '../models/books';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest, map } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-books',
@@ -15,12 +18,19 @@ import { MatButtonModule } from '@angular/material/button';
     MatTableModule,
     MatButtonModule,
     RouterModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css',
 })
 export class BooksComponent {
   books$!: Observable<Book[]>;
+
+  private allBooks$ = new BehaviorSubject<Book[]>([]);
+  searchTerm$ = new BehaviorSubject<string>('');
+
   displayedColumns: string[] = [
     'cover_image',
     'author', 
@@ -31,6 +41,26 @@ export class BooksComponent {
 
   constructor(private bookService: BookService) {
     this.books$ = this.bookService.getAll();
+  }
+
+  ngOnInit() {
+    this.bookService.getAll().subscribe((books) => {
+      this.allBooks$.next(books);
+    });
+
+    // book filter by title or author
+    this.books$ = combineLatest([this.allBooks$, this.searchTerm$]).pipe(
+      map(([books, term]) =>
+        books.filter((book) =>
+          book.title.toLowerCase().includes(term.toLowerCase()) ||
+          book.author.toLowerCase().includes(term.toLowerCase())
+        )
+      )
+    );
+  }
+
+  onSearchChange(value: string) {
+    this.searchTerm$.next(value);
   }
 
   openBook(bookId: number) {
